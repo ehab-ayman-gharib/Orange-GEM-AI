45cc976a1dad6401eaaaf9578065260ca3159706
Ehab Ayman <>
2025-10-31 00:09:40 +0200

    repo name fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/Orange-Gem-AI/', // Replace with your actual GitHub repo name
+    base: '/Orange-GEM-AI/', // Replace with your actual GitHub repo name
 });
